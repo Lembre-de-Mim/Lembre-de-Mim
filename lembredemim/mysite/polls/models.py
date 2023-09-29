13c5179cee83cdebe2d1f338b21621a4b23a8dcd
Francisco Soares <145678243+francsilv@users.noreply.github.com>
2023-09-29 19:30:27 -0300

    Update models.py
    foi adicionada a linha de 16 e 17 para  teste

--- lembredemim/mysite/polls/models.py
+++ lembredemim/mysite/polls/models.py
@@ -5,8 +5,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Data de publicação')
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     vote = models.IntegerField(default=0)
 # Create your models here.
+
+
+class index(models.Model):
+    Question
